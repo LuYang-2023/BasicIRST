a17f41e2f8f0da7e405baddc020c4108a94add68
我运气超好 <yj20220275@stud.tjut.edu.cn>
2024-06-11 09:49:47 +0800

    Update model_DNANet.py

--- model/DNANet/model_DNANet.py
+++ model/DNANet/model_DNANet.py
@@ -2,123 +2,126 @@ import torch
 import torch.nn as nn
 
 
-class SimAM(torch.nn.Module):
-    def __init__(self, channels=None, e_lambda=1e-5):
-        super(SimAM, self).__init__()
-
-        self.activaton = nn.Sigmoid()
-        self.e_lambda = e_lambda
-
-    def __repr__(self):
-        s = self.__class__.__name__ + '('
-        s += ('lambda=%f)' % self.e_lambda)
-        return s
-
-    @staticmethod
-    def get_module_name():
-        return "simam"
-
-    def forward(self, x):
-        b, c, h, w = x.size()
-
-        n = w * h - 1
-
-        x_minus_mu_square = (x - x.mean(dim=[2, 3], keepdim=True)).pow(2)
-        y = x_minus_mu_square / (4 * (x_minus_mu_square.sum(dim=[2, 3], keepdim=True) / n + self.e_lambda)) + 0.5
-
-        return x * self.activaton(y)
-
-
-class Res_SimAM_block(nn.Module):
-    expansion = 1
-
-    def __init__(self, in_channels, out_channels, stride=1):
-        super(Res_SimAM_block, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+class VGG_CBAM_Block(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, padding=1)
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, padding=1)
         self.bn2 = nn.BatchNorm2d(out_channels)
-        # self.conv2 = nn.Sequential(self.conv2,SimAM(out_channels))
-        if stride != 1 or out_channels != in_channels:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride),
-                nn.BatchNorm2d(out_channels)
-            )
-        else:
-            self.shortcut = None
-
-        self.simam = SimAM(out_channels)
-
-
+        self.relu = nn.ReLU(inplace=True)
+        self.ca = ChannelAttention(out_channels)
+        self.sa = SpatialAttention()
 
     def forward(self, x):
-
-        residual = x
-        if self.shortcut is not None:
-            residual = self.shortcut(x)
-
         out = self.conv1(x)
-
         out = self.bn1(out)
         out = self.relu(out)
         out = self.conv2(out)
         out = self.bn2(out)
-        out = self.simam(out)
-
-        out += residual
+        out = self.ca(out) * out
+        out = self.sa(out) * out
         out = self.relu(out)
-
         return out
 
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.fc1   = nn.Conv2d(in_planes, in_planes // 16, 1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2   = nn.Conv2d(in_planes // 16, in_planes, 1, bias=False)
+        self.sigmoid = nn.Sigmoid()
+    def forward(self, x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
+
+class Res_CBAM_block(nn.Module):
+    def __init__(self, in_channels, out_channels, stride = 1):
+        super(Res_CBAM_block, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = stride, padding = 1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace = True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size = 3, padding = 1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        if stride != 1 or out_channels != in_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size = 1, stride = stride),
+                nn.BatchNorm2d(out_channels))
+        else:
+            self.shortcut = None
+
+        self.ca = ChannelAttention(out_channels)
+        self.sa = SpatialAttention()
+
+    def forward(self, x):
+        residual = x
+        if self.shortcut is not None:
+            residual = self.shortcut(x)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.ca(out) * out
+        out = self.sa(out) * out
+        out += residual
+        out = self.relu(out)
+        return out
 
 class DNANet(nn.Module):
-    def __init__(self, num_classes=1, input_channels=1, block=Res_SimAM_block, num_blocks=[3, 4, 6, 3], nb_filter=[16, 32, 64, 128, 256], deep_supervision=True, mode='test'):
+    def __init__(self, num_classes=1,input_channels=1, block=Res_CBAM_block, num_blocks=[2, 2, 2, 2], nb_filter=[16, 32, 64, 128, 256], deep_supervision=True, mode='test'):
         super(DNANet, self).__init__()
         self.mode = mode
-        self.fmap_block = dict()  # 装feature map
-        self.grad_block = dict()  # 装梯度
-
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU(inplace = True)
         self.deep_supervision = deep_supervision
-        self.pool = nn.MaxPool2d(2, 2)
-        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        self.down = nn.Upsample(scale_factor=0.5, mode='bilinear', align_corners=True)
+        self.pool  = nn.MaxPool2d(2, 2)
+        self.up    = nn.Upsample(scale_factor=2,   mode='bilinear', align_corners=True)
+        self.down  = nn.Upsample(scale_factor=0.5, mode='bilinear', align_corners=True)
 
-        self.up_4 = nn.Upsample(scale_factor=4, mode='bilinear', align_corners=True)
-        self.up_8 = nn.Upsample(scale_factor=8, mode='bilinear', align_corners=True)
-        self.up_16 = nn.Upsample(scale_factor=16, mode='bilinear', align_corners=True)
-        # 第一列
+        self.up_4  = nn.Upsample(scale_factor=4,   mode='bilinear', align_corners=True)
+        self.up_8  = nn.Upsample(scale_factor=8,   mode='bilinear', align_corners=True)
+        self.up_16 = nn.Upsample(scale_factor=16,  mode='bilinear', align_corners=True)
+
         self.conv0_0 = self._make_layer(block, input_channels, nb_filter[0])
-        self.conv1_0 = self._make_layer(block, nb_filter[0], nb_filter[1], num_blocks[0])
-        self.conv2_0 = self._make_layer(block, nb_filter[1], nb_filter[2], num_blocks[1])
-        self.conv3_0 = self._make_layer(block, nb_filter[2], nb_filter[3], num_blocks[2])
-        self.conv4_0 = self._make_layer(block, nb_filter[3], nb_filter[4], num_blocks[3])
-        # 第二列
-        self.conv0_1 = self._make_layer(block, nb_filter[0] + nb_filter[1], nb_filter[0])
-        self.conv1_1 = self._make_layer(block, nb_filter[1] + nb_filter[2] + nb_filter[0], nb_filter[1], num_blocks[0])
-        self.conv2_1 = self._make_layer(block, nb_filter[2] + nb_filter[3] + nb_filter[1], nb_filter[2], num_blocks[1])
-        self.conv3_1 = self._make_layer(block, nb_filter[3] + nb_filter[4] + nb_filter[2], nb_filter[3], num_blocks[2])
-        # 更深一个节点
-        self.conv4_1 = self._make_layer(block, nb_filter[3], nb_filter[4], num_blocks[3])
+        self.conv1_0 = self._make_layer(block, nb_filter[0],  nb_filter[1], num_blocks[0])
+        self.conv2_0 = self._make_layer(block, nb_filter[1],  nb_filter[2], num_blocks[1])
+        self.conv3_0 = self._make_layer(block, nb_filter[2],  nb_filter[3], num_blocks[2])
+        self.conv4_0 = self._make_layer(block, nb_filter[3],  nb_filter[4], num_blocks[3])
 
-        # 第三列
-        self.conv2_2 = self._make_layer(block, nb_filter[2] * 2 + nb_filter[3], nb_filter[2])
-        self.conv3_2 = self._make_layer(block, nb_filter[3] * 2 + nb_filter[4] + nb_filter[2], nb_filter[3],
-                                        num_blocks[0])
-        # 第四列
-        self.conv1_2 = self._make_layer(block, nb_filter[1] * 2 + nb_filter[2], nb_filter[1])
-        self.conv2_3 = self._make_layer(block, nb_filter[2] * 3 + nb_filter[3] + nb_filter[1], nb_filter[2],
-                                        num_blocks[0])
-        # 第五列
-        self.conv0_2 = self._make_layer(block, nb_filter[0] * 2 + nb_filter[1], nb_filter[0])
-        self.conv1_3 = self._make_layer(block, nb_filter[1] * 3 + nb_filter[2] + nb_filter[0], nb_filter[1],
-                                        num_blocks[0])
-        # 第六列
-        self.conv0_3 = self._make_layer(block, nb_filter[0] * 3 + nb_filter[1], nb_filter[0])  #
-        # 
-        self.conv0_4_final = self._make_layer(block, nb_filter[0] * 5, nb_filter[0])
+        self.conv0_1 = self._make_layer(block, nb_filter[0] + nb_filter[1],  nb_filter[0])
+        self.conv1_1 = self._make_layer(block, nb_filter[1] + nb_filter[2] + nb_filter[0],  nb_filter[1], num_blocks[0])
+        self.conv2_1 = self._make_layer(block, nb_filter[2] + nb_filter[3] + nb_filter[1],  nb_filter[2], num_blocks[1])
+        self.conv3_1 = self._make_layer(block, nb_filter[3] + nb_filter[4] + nb_filter[2],  nb_filter[3], num_blocks[2])
 
+        self.conv0_2 = self._make_layer(block, nb_filter[0]*2 + nb_filter[1], nb_filter[0])
+        self.conv1_2 = self._make_layer(block, nb_filter[1]*2 + nb_filter[2]+ nb_filter[0], nb_filter[1], num_blocks[0])
+        self.conv2_2 = self._make_layer(block, nb_filter[2]*2 + nb_filter[3]+ nb_filter[1], nb_filter[2], num_blocks[1])
+
+        self.conv0_3 = self._make_layer(block, nb_filter[0]*3 + nb_filter[1], nb_filter[0])
+        self.conv1_3 = self._make_layer(block, nb_filter[1]*3 + nb_filter[2]+ nb_filter[0], nb_filter[1], num_blocks[0])
+
+        self.conv0_4 = self._make_layer(block, nb_filter[0]*4 + nb_filter[1], nb_filter[0])
+
+        self.conv0_4_final = self._make_layer(block, nb_filter[0]*5, nb_filter[0])
 
         self.conv0_4_1x1 = nn.Conv2d(nb_filter[4], nb_filter[0], kernel_size=1, stride=1)
         self.conv0_3_1x1 = nn.Conv2d(nb_filter[3], nb_filter[0], kernel_size=1, stride=1)
@@ -126,33 +129,17 @@ class DNANet(nn.Module):
         self.conv0_1_1x1 = nn.Conv2d(nb_filter[1], nb_filter[0], kernel_size=1, stride=1)
 
         if self.deep_supervision:
-            self.final1 = nn.Conv2d(nb_filter[0], num_classes, kernel_size=1)
-            self.final2 = nn.Conv2d(nb_filter[0], num_classes, kernel_size=1)
-            self.final3 = nn.Conv2d(nb_filter[0], num_classes, kernel_size=1)
-            self.final4 = nn.Conv2d(nb_filter[0], num_classes, kernel_size=1)
+            self.final1 = nn.Conv2d (nb_filter[0], num_classes, kernel_size=1)
+            self.final2 = nn.Conv2d (nb_filter[0], num_classes, kernel_size=1)
+            self.final3 = nn.Conv2d (nb_filter[0], num_classes, kernel_size=1)
+            self.final4 = nn.Conv2d (nb_filter[0], num_classes, kernel_size=1)
         else:
-            self.final = nn.Conv2d(nb_filter[0], num_classes, kernel_size=1)
+            self.final  = nn.Conv2d (nb_filter[0], num_classes, kernel_size=1)
 
-        #self.final4.register_backward_hook(self.save_grad)
-
-    def save_grad(self, module, grad_input, grad_output):
-        self.grad_block['final2_gradin'] = grad_input
-        self.grad_block['final2_gradout'] = grad_output
-
-    def forward_hook(self, module, input, output):
-        self.fmap_block['input'] = input
-        self.fmap_block['output'] = output
-        print('#' * 20)
-
-    def backward_hook(self, module, grad_in, grad_out):
-        self.grad_block['grad_in'] = grad_in
-        self.grad_block['grad_out'] = grad_out
-        print('%' * 20)
-
-    def _make_layer(self, block, input_channels, output_channels, num_blocks=1):
+    def _make_layer(self, block, input_channels,  output_channels, num_blocks=1):
         layers = []
         layers.append(block(input_channels, output_channels))
-        for i in range(num_blocks - 1):
+        for i in range(num_blocks-1):
             layers.append(block(output_channels, output_channels))
         return nn.Sequential(*layers)
 
@@ -162,26 +149,23 @@ class DNANet(nn.Module):
         x0_1 = self.conv0_1(torch.cat([x0_0, self.up(x1_0)], 1))
 
         x2_0 = self.conv2_0(self.pool(x1_0))
-        x1_1 = self.conv1_1(torch.cat([x1_0, self.up(x2_0), self.down(x0_1)], 1))
+        x1_1 = self.conv1_1(torch.cat([x1_0, self.up(x2_0),self.down(x0_1)], 1))
+        x0_2 = self.conv0_2(torch.cat([x0_0, x0_1, self.up(x1_1)], 1))
 
         x3_0 = self.conv3_0(self.pool(x2_0))
-        x2_1 = self.conv2_1(torch.cat([x2_0, self.up(x3_0), self.down(x1_1)], 1))
+        x2_1 = self.conv2_1(torch.cat([x2_0, self.up(x3_0),self.down(x1_1)], 1))
+        x1_2 = self.conv1_2(torch.cat([x1_0, x1_1, self.up(x2_1),self.down(x0_2)], 1))
+        x0_3 = self.conv0_3(torch.cat([x0_0, x0_1, x0_2, self.up(x1_2)], 1))
 
         x4_0 = self.conv4_0(self.pool(x3_0))
-        x3_1 = self.conv3_1(torch.cat([x3_0, self.up(x4_0), self.down(x2_1)], 1))
-        x2_2 = self.conv2_2(torch.cat([x2_0, x2_1, self.up(x3_1)], 1))
-        x1_2 = self.conv1_2(torch.cat([x1_0, x1_1, self.up(x2_2)], 1))
-        x0_2 = self.conv0_2(torch.cat([x0_0, x0_1, self.up(x1_2)], 1))
-
-        x4_1 = self.conv4_1(self.down(x3_1))
-        x3_2 = self.conv3_2(torch.cat([x3_0, x3_1, self.up(x4_1), self.down(x2_2)], 1))
-        x2_3 = self.conv2_3(torch.cat([x2_0, x2_1, x2_2, self.up(x3_2), self.down(x1_2)], 1))
-        x1_3 = self.conv1_3(torch.cat([x1_0, x1_1, x1_2, self.up(x2_3), self.down(x0_2)], 1))
-        x0_3 = self.conv0_3(torch.cat([x0_0, x0_1, x0_2, self.up(x1_3)], 1))
+        x3_1 = self.conv3_1(torch.cat([x3_0, self.up(x4_0),self.down(x2_1)], 1))
+        x2_2 = self.conv2_2(torch.cat([x2_0, x2_1, self.up(x3_1),self.down(x1_2)], 1))
+        x1_3 = self.conv1_3(torch.cat([x1_0, x1_1, x1_2, self.up(x2_2),self.down(x0_3)], 1))
+        x0_4 = self.conv0_4(torch.cat([x0_0, x0_1, x0_2, x0_3, self.up(x1_3)], 1))
 
         Final_x0_4 = self.conv0_4_final(
-            torch.cat([self.up_16(self.conv0_4_1x1(x4_1)), self.up_8(self.conv0_3_1x1(x3_2)),
-                       self.up_4(self.conv0_2_1x1(x2_3)), self.up(self.conv0_1_1x1(x1_3)), x0_3], 1))
+            torch.cat([self.up_16(self.conv0_4_1x1(x4_0)),self.up_8(self.conv0_3_1x1(x3_1)),
+                       self.up_4 (self.conv0_2_1x1(x2_2)),self.up  (self.conv0_1_1x1(x1_3)), x0_4], 1))
 
         if self.deep_supervision:
             output1 = self.final1(x0_1).sigmoid()
